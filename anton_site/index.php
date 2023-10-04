381217df0101a7a7bef821c57987462720d6db82
ivan <ivan@ubuntu2.myguest.virtualbox.org>
2023-10-04 13:56:58 +0300

    add nginx conf

---
+++ anton_site/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Anton's Site</title>
+</head>
+<body>
+    <h1 style="color: blue;">Anton</h1>
+</body>
+</html>
---
+++ ivan_site/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ivan's Site</title>
+</head>
+<body>
+    <h1 style="color: yellow;">Ivan</h1>
+</body>
+</html>
